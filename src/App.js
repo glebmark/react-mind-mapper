f5b675a28f9a871eaee3b047f1f3a71e75081b8a
glebmark <sechastwothree@protonmail.com>
2022-05-10 11:10:24 +0400

    added seCallback to handleUndoRedo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 
 function Node(props) {    
@@ -20,36 +20,38 @@ function Node(props) {
         };
     
     return (
-        <div
+        <textarea
             style={styleSquare}
             className="node" 
             onClick={props.onClick} 
             draggable="true"
             onDrag={props.onDrag}
             onDragStart={props.onDragStart}
+            overflow="visible"
+            // disabled="true"
             // onPointerMove={props.onDrag}
+
+            // contentEditable="true"
         >
             {props.value} 
-        </div>
+        </textarea>
         );
   }
 
 
-class Area extends React.Component {
+function Area(props){
 
-    render() { 
-
-        const currentNodes = this.props.nodes.map((v, nodeNumber) => {
+        const currentNodes = props.nodes.map((v, nodeNumber) => {
             // console.log("THIS IS this.props.nodes: " + this.props.nodes)
             return (
                 <Node 
                     key={nodeNumber}
-                    // value={this.props.nodes[i]}
-                    clientY={this.props.nodes[nodeNumber].clientY}
-                    clientX={this.props.nodes[nodeNumber].clientX}
-                    onClick={() => {this.props.onClick(nodeNumber);}} 
-                    onDrag={dragEvent => this.props.onDrag(nodeNumber, dragEvent)}
-                    onDragStart={dragStartEvent => this.props.onDragStart(nodeNumber, dragStartEvent)}
+                    // value={props.nodes[i]}
+                    clientY={props.nodes[nodeNumber].clientY}
+                    clientX={props.nodes[nodeNumber].clientX}
+                    onClick={() => {props.onClick(nodeNumber);}} 
+                    onDrag={dragEvent => props.onDrag(nodeNumber, dragEvent)}
+                    onDragStart={dragStartEvent => props.onDragStart(nodeNumber, dragStartEvent)}
                 />
             );
         });
@@ -68,58 +70,82 @@ class Area extends React.Component {
             <div
                 id='area' 
                 style={styleArea} 
-                onDoubleClick={mouseEvent => this.props.onDoubleClick(mouseEvent)}
+                onDoubleClick={mouseEvent => props.onDoubleClick(mouseEvent)}
             >
                 {currentNodes}
             </div>
         );
-    }
 }
 
 
-class MindMap extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-              history: [
-                {
-                  nodes: [],
-                }
-              ],
-              moveNumber: 0,
-          }
-      }
+
+function MindMap() {
+    const [history, setHistory] = useState([{nodes: []}]);
+    const [moveNumber, setMoveNumber] = useState(0);
+        
     
-    jumpTo(moveNumber) {
-        this.setState({
-            moveNumber: moveNumber,
-        })
+
+    // useEffect(() => {
+    //     document.addEventListener("keydown", handleUndoRedo(moveNumber, keyEvent));
+    //     return() => {
+    //         document.removeEventListener("keydown", handleUndoRedo(moveNumber, keyEvent));
+    //     }
+    // })
+    
+
+
+    function jumpTo(moveNumber) {
+        setMoveNumber(moveNumber)
     }
 
+    const handleUndoRedo = useCallback(keyEvent => {
+        console.log(keyEvent)
+        // console.log("history.length: " + history.length)
+        // console.log("moveNumber: " + moveNumber)
+        
+        if((keyEvent.ctrlKey || keyEvent.metaKey) && !keyEvent.shiftKey && keyEvent.key === 'z' && moveNumber !== 0) {
+            console.log("ctrl+Z")
+            setMoveNumber(moveNumber - 1) // undo
+        }
+        
+        if(history.length - 1 > moveNumber) {
+            if (keyEvent.ctrlKey && keyEvent.key === 'y') { // Windows Linux
+                setMoveNumber(moveNumber + 1) // redo
+            } else if (keyEvent.metaKey && keyEvent.shiftKey && (keyEvent.key === 'z' || keyEvent.key === 'Z')) { // macOS
+                setMoveNumber(moveNumber + 1) // redo
+            }
+            console.log("meta shift Z")
+            // } else if (keyEvent.keyCode === 90) { // macOS
+            //     console.log("meta shift Z")
+            //     setMoveNumber(moveNumber + 1) // redo
+            // }
+        }
+    }, [moveNumber]); // on update of moveNumber
 
-    handleClick(i) {
-        console.log("one click")
-        const history = this.state.history.slice(0, this.state.moveNumber + 1); // +1 because end not included in slice
-        const historyCopy = [...this.state.history];
+    useEffect(() => {
+        window.addEventListener("keydown", handleUndoRedo);
+        return() => {
+            window.removeEventListener("keydown", handleUndoRedo);
+        }
+    }, [handleUndoRedo])
+
+    function handleClick(i) {
+        // console.log("one click")
+        const historyCopy = history.slice(0, moveNumber + 1); // +1 because end not included in slice
         const current = historyCopy[historyCopy.length - 1]; 
         const nodes = [...current.nodes];
 
-
-        this.setState({
-            history: historyCopy.concat([{
-                nodes: nodes,
-            }]),
-            moveNumber: historyCopy.length,
-        })
+        setHistory(historyCopy.concat([{nodes: nodes}]));
+        setMoveNumber(historyCopy.length)
     }
 
 
-    handleDoubleClick(mouseEvent) {
-        const historyCopy = this.state.history.slice(0, this.state.moveNumber + 1); // +1 because end not included in slice; .slice instead of ... should be used because we have to truncate up to moveNumber to discard all needn't moves (it's for undo)
+    function handleDoubleClick(mouseEvent) {
+        const historyCopy = history.slice(0, moveNumber + 1); // +1 because end not included in slice; .slice instead of ... should be used because we have to truncate up to moveNumber to discard all needn't moves (it's for undo)
         const current = historyCopy[historyCopy.length - 1]; 
         const nodes = [...current.nodes];
 
-        console.log(mouseEvent)
+        // console.log(mouseEvent)
         
         if (mouseEvent.clientY <= 25 || mouseEvent.clientX <= 25) {
             return
@@ -131,18 +157,13 @@ class MindMap extends React.Component {
             }
             nodes[nodes.length] = node;
     
-            this.setState({
-                history: historyCopy.concat([{
-                    nodes: nodes,
-                }]),
-                moveNumber: historyCopy.length,
-            })
+            setHistory(historyCopy.concat([{nodes: nodes}]));
+            setMoveNumber(historyCopy.length)
         }
-
     }
 
 
-    handleOnDrag(nodeNumber, dragEvent) {
+    function handleOnDrag(nodeNumber, dragEvent) {
         const clientY = dragEvent.clientY;
         const clientX = dragEvent.clientX;
 
@@ -151,25 +172,27 @@ class MindMap extends React.Component {
             return
         }
 
-        const historyCopy = [...this.state.history]; // there .slice isn't used as there no need to store new moves
+        const historyCopy = [...history]; // there .slice isn't used as there no need to store new moves
         historyCopy[historyCopy.length - 1].nodes[nodeNumber].clientY = clientY;
         historyCopy[historyCopy.length - 1].nodes[nodeNumber].clientX = clientX;
-        this.setState({
-            history: historyCopy
-        })
+
+        setHistory(historyCopy)
     }
 
 
-    handleOnDragStart(nodeNumber, dragStartEvent) {
+    // this handle serves several goals:
+    // 1) it hides draggable shadow
+    // 2) it adds new move for making history of states
+    function handleOnDragStart(nodeNumber, dragStartEvent) {
         let dragImg = new Image(0,0);
         dragImg.src ='data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
         dragStartEvent.dataTransfer.setDragImage(dragImg, 0, 0);
 
-        const historyCopy = this.state.history.slice(0, this.state.moveNumber + 1); // +1 because end not included in slice
+        const historyCopy = history.slice(0, moveNumber + 1); // +1 because end not included in slice
         const current = historyCopy[historyCopy.length - 1]; 
         const nodes = [...current.nodes];
 
-        console.log(dragStartEvent)
+        // console.log(dragStartEvent)
         
         let nodeAlikeState = {
             clientY: dragStartEvent.clientY,
@@ -177,48 +200,43 @@ class MindMap extends React.Component {
         }
         nodes[nodeNumber] = nodeAlikeState; // it's not new node as in handeDoubleClick, it's rather state (new coordinates) of previous one
 
-        this.setState({
-            history: historyCopy.concat([{
-                nodes: nodes,
-            }]),
-            moveNumber: historyCopy.length,
-        })
+        setHistory(historyCopy.concat([{nodes: nodes}]));
+        setMoveNumber(historyCopy.length)
     }
 
 
-    render() {
-        // console.log(this.state.history)
-        const history = this.state.history;
-        const current = history[this.state.moveNumber];
-
-
-        const moves = history.map((step, moveNumber) => {
-            return (
-                <li key={moveNumber}>
-                    <button onClick={() => this.jumpTo(moveNumber)}>Jump to move: {moveNumber}</button>
-                </li>
-            );
-        });
-
-        
+    const moves = history.map((step, moveNumber) => {
         return (
-            <div className='mindmap'>
+            <li key={moveNumber}>
+                <button onClick={() => jumpTo(moveNumber)}>Jump to move: {moveNumber}</button>
+            </li>
+        );
+    });
 
-                    <Area 
-                        nodes={current.nodes}
-                        // onClick={(i) => {this.handleClick(i), this.handleClick(i)}}
-                        onClick={i => {this.handleClick(i);}}
-                        onDoubleClick={(mouseEvent) => {this.handleDoubleClick(mouseEvent);}}
-                        onDrag={(nodeNumber, dragEvent) => this.handleOnDrag(nodeNumber, dragEvent)}
-                        onDragStart={(nodeNumber, dragStartEvent) => this.handleOnDragStart(nodeNumber, dragStartEvent)}
-                    />
 
-                <ul>
-                    {moves}
-                </ul>
-            </div>
-          );
-    }
+    return (
+        <div 
+            className='mindmap'
+            onKeyDown={(keyEvent) => handleUndoRedo(moveNumber, keyEvent)}
+        >
+
+                <Area 
+                    nodes={history[moveNumber].nodes}
+                    // onClick={(i) => {this.handleClick(i), this.handleClick(i)}}
+                    // onClick={i => handleClick(i)}
+                    onDoubleClick={mouseEvent => handleDoubleClick(mouseEvent)}
+                    onDrag={(nodeNumber, dragEvent) => handleOnDrag(nodeNumber, dragEvent)}
+                    onDragStart={(nodeNumber, dragStartEvent) => handleOnDragStart(nodeNumber, dragStartEvent)}
+                />
+
+            <ul>
+                {moves}
+            </ul>
+        </div>
+        );
 }
 
+
 export default MindMap;
+
+
