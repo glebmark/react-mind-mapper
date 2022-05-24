9a862ca0f904148e17c8089abfb0793646bb2498
glebmark <sechastwothree@protonmail.com>
2022-05-24 14:21:25 +0400

    added conditional rendering for Editor

--- src/element.js
+++ src/element.js
@@ -28,63 +28,21 @@ export function Node(props) {
         };
     
 
-    
-    // const editor = useRef(null);
     const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
     
     const editorStateBlocks = convertToRaw(editorState.getCurrentContent()).blocks;
     const editorStateValue = editorStateBlocks.map(block => (!block.text.trim() && '\n') || block.text).join('\n');
 
 
-    const boldText = (e) => {        
-        // onMouseDown and e.preventDefault because editor losses focus if you use onClick
-        e.preventDefault();
-        let nextState = RichUtils.toggleInlineStyle(editorState, 'BOLD');
-        setEditorState(nextState);
-    }
-    const underlineText = (e) => {        
-        // onMouseDown and e.preventDefault because editor losses focus if you use onClick
-        e.preventDefault();
-        let nextState = RichUtils.toggleInlineStyle(editorState, 'UNDERLINE');
-        setEditorState(nextState);
-    }
-
-    const italicText = (e) => {        
-        // onMouseDown and e.preventDefault because editor losses focus if you use onClick
-        e.preventDefault();
-        let nextState = RichUtils.toggleInlineStyle(editorState, 'ITALIC');
-        setEditorState(nextState);
-    }
-
-    const alignmentRight = (e) => {        
-        // onMouseDown and e.preventDefault because editor losses focus if you use onClick
-        e.preventDefault();
-        let nextState = RichUtils.toggleInlineStyle(editorState, 'RIGHT');
-        setEditorState(nextState);
-    }
-
-    const handleKeyCommand = command => {
-        const newState = RichUtils.handleKeyCommand(editorState, command);
-        if (newState) {
-        //   onChange(newState);
-            setEditorState(newState)
-            return "handled";
-        }
-        return "not-handled";
-      };
-    
-
     const innerRef = useRef(null);  
-
-    // let isLoggedIn = false;
-    let [isHover, setIsHover] = React.useState(false);
+    let [isClicked, setIsClicked] = React.useState(false);
 
     const handleClick = () => {
         console.log("focus")
-        // isHover = true;
-        setIsHover(true)
+        // isClicked = true;
+        setIsClicked(true)
     }
-    
+
     useEffect(() => {
         const div = innerRef.current;
         div.addEventListener("click", handleClick);
@@ -93,88 +51,43 @@ export function Node(props) {
         }
     }, [handleClick])
     
-
-    console.log(isHover)
-    switch (isHover) {
-        case true:
-            return (
-                <div
-                    style={styleSquare}
-                    className="node" 
-                    // onClick={props.onClick} 
-                    // onClick={handleOnClick} 
-                    draggable="true"
-                    onDrag={props.onDrag}
-                    onDragStart={props.onDragStart}
-                    // onInput={(e) => props.onTextChange(e)}
-                    // overflow="visible"
-                    // disabled="true"
-                    // onPointerMove={props.onDrag}
-                    
-                    // contentEditable="true"
-                    // suppressContentEditableWarning={true}
-                    ref={innerRef}
-                    value = {props.innerText}
-                    
-                >
-                    <div className='editor'>
-                        {/* <button onMouseDown = {boldText}>B</button>
-                        <button onMouseDown = {italicText}>I</button>
-                        <button onMouseDown = {underlineText}>U</button>
-                        <button onMouseDown = {alignmentRight}>R1</button> */}
-                        {/* <button onMouseDown={() => applyAlignment('left')}>L</button>
-                        <button onMouseDown={() => applyAlignment('center')}>C</button>
-                        <button onMouseDown={() => applyAlignment('right')}>R</button> */}
-                        
+    let divEditor = <div className='editor'>  
                         <Editor 
                             editorState={editorState} 
                             onChange={(e) => {
-                                // setEditorState(e);
                                 props.onTextChange(editorStateValue);
                                 }}
-        
                             onEditorStateChange={setEditorState}
                             // handleKeyCommand={handleKeyCommand}
                             // plugins={plugins}
                             // ref={editor}
                             // blockStyleFn={blockStyleFn}
-        
                             // placeholder='dsfdsf'
                             />
-                        
                     </div>
-                </div>
-                );
-          break;
-        case false:
-            return (
-                <div
-                    style={styleSquare}
-                    className="node" 
-                    // onClick={props.onClick} 
-                    // onClick={handleOnClick} 
-                    draggable="true"
-                    onDrag={props.onDrag}
-                    onDragStart={props.onDragStart}
-                    // onInput={(e) => props.onTextChange(e)}
-                    // overflow="visible"
-                    // disabled="true"
-                    // onPointerMove={props.onDrag}
-                    
-                    // contentEditable="true"
-                    // suppressContentEditableWarning={true}
-                    ref={innerRef}
-                    value = {props.innerText}
-                >
-                </div>
-                );
-          break;
-        default:
-          return null;
-      }
-    
 
+    console.log(isClicked)
 
-
-    
+    return (
+            <div
+                style={styleSquare}
+                className="node" 
+                // onClick={props.onClick} 
+                // onClick={handleOnClick} 
+                draggable="true"
+                onDrag={props.onDrag}
+                onDragStart={props.onDragStart}
+                // onInput={(e) => props.onTextChange(e)}
+                // overflow="visible"
+                // disabled="true"
+                // onPointerMove={props.onDrag}
+                
+                // contentEditable="true"
+                // suppressContentEditableWarning={true}
+                ref={innerRef}
+                value = {props.innerText}
+            >
+                {isClicked ? divEditor : null}
+            </div>
+        );
 }
